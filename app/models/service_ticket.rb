34115157cf3524c46685ea79dc090dfceba28f7f
Michael He <hlxwell@gmail.com>
2011-11-12 22:53:18 +0800

    remove service_callback_url.

--- app/models/service_ticket.rb
+++ app/models/service_ticket.rb
@@ -2,7 +2,7 @@ class ServiceTicket < ActiveRecord::Base
   include Consumable
   include TicketLike
 
-  SERVICES = ["http://localhost:3000/auth/cas/callback"]
+  SERVICES = ["en.lvh.me", "us.lvh.me"]
 
   belongs_to :granted_by_tgt,
              :class_name => 'TicketGrantingTicket',
@@ -18,9 +18,5 @@ class ServiceTicket < ActiveRecord::Base
 
   def valid_for_service? service_name
     self.service == service_name
-  end
-
-  def service_callback_url
-    "#{self.service}?ticket=#{ticket}"
   end
 end
