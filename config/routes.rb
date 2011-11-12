d7d171de67e057c0f4f36a636957a2890d11d603
Michael He <hlxwell@gmail.com>
2011-11-12 22:02:33 +0800

    add validator route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 UserCenter::Application.routes.draw do
-  root :to => 'users#index'
-    
+  root :to => 'user_sessions#new'
+
   resources :users, :admins do
     collection do
       get :login_from_http_basic
@@ -9,17 +9,20 @@ UserCenter::Application.routes.draw do
       get :activate
     end
   end
-  
+
   resources :user_sessions
   resources :password_resets
-  
+
   match 'login' => 'user_sessions#new', :as => :login
   match 'logout' => 'user_sessions#destroy', :as => :logout
-  
+
   resource :oauth do
     get :callback
   end
   match "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
+
+  match "/serviceValidate" => "validators#serviceValidate"
+  # match "/proxyValidate" => "validators#proxyValidate"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
