2af772f52460138529745c53ce63f7c35ed06f2d
Michael He <hlxwell@gmail.com>
2011-11-12 22:54:30 +0800

    add issue service ticket and has_valid_tgt methods to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :require_login, :except => [:not_authenticated]
-
+  # before_filter :require_login, :except => [:not_authenticated]
   helper_method :current_users_list
-
 
   def render_validation_error(code, message = nil)
     xml = Nokogiri::XML::Builder.new do |xml|
@@ -23,14 +21,40 @@ class ApplicationController < ActionController::Base
         xml.parent.namespace = xml.parent.namespace_definitions.first
         xml['cas'].authenticationSuccess {
           xml['cas'].user username
-          append_user_info(username, xml)
+          # append_user_info(username, xml)
         }
       }
     end
     render :xml => xml.to_xml
   end
 
+  def get_url_host url
+    URI::parse(CGI.unescape(url.to_s)).host
+  end
+
   protected
+
+  def has_valid_tgt
+    TicketGrantingTicket.where(:ticket => cookies[:tgt]).unconsumed.first
+  end
+
+  # issue a Service Ticket and redirect back
+  def issue_service_ticket
+    if tgt = has_valid_tgt and cookies[:service] and cookies[:service_back_url]
+      # TODO need to verify params[:service] included in "available services"
+      st = ServiceTicket.create(
+        :service => cookies[:service],
+        :username => current_user.id,
+        :granted_by_tgt_id => tgt.id
+      )
+      back_url = cookies[:service_back_url]
+      cookies.delete :service
+      cookies.delete :service_back_url
+
+      redirect_to back_url
+      return
+    end
+  end
 
   def not_authenticated
     redirect_to root_path, :alert => "Please login first."
