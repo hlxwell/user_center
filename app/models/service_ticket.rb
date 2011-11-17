cb41d4a7f4d889d4bee5dc7e75a63f7d5b6f767f
Michael He <hlxwell@gmail.com>
2011-11-18 00:10:28 +0800

    change service urls

--- app/models/service_ticket.rb
+++ app/models/service_ticket.rb
@@ -3,40 +3,9 @@ class ServiceTicket < ActiveRecord::Base
   include TicketLike
 
   SERVICES = [
-    "en.local.theplant-dev.com",
-    "127.0.0.1",
-    "en.lvh.me",
-    "us.lvh.me",
-    "nl.lvh.me",
-    "uk.lvh.me",
-    "fr.lvh.me",
-    "de.lvh.me",
-    "a.lvh.me",
-    "b.lvh.me",
-    "c.lvh.me",
-    "d.lvh.me",
-    "e.lvh.me",
-    "f.lvh.me",
-    "g.lvh.me",
-    "h.lvh.me",
-    "i.lvh.me",
-    "j.lvh.me",
-    "k.lvh.me",
-    "l.lvh.me",
-    "m.lvh.me",
-    "n.lvh.me",
-    "o.lvh.me",
-    "p.lvh.me",
-    "q.lvh.me",
-    "r.lvh.me",
-    "s.lvh.me",
-    "t.lvh.me",
-    "u.lvh.me",
-    "v.lvh.me",
-    "w.lvh.me",
-    "x.lvh.me",
-    "y.lvh.me",
-    "z.lvh.me"
+    "a.client.sso.theplant-dev.com",
+    "b.client.sso.theplant-dev.com",
+    "c.client.sso.theplant-dev.com"
   ]
 
   belongs_to :granted_by_tgt,
