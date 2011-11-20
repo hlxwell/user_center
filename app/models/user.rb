f83412e185cf8b761836efc267f68addb4d100df
Michael He <hlxwell@gmail.com>
2011-11-20 22:12:31 +0800

    add email uniqueness validtor for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ActiveRecord::Base
   has_many :providers, :class_name => "UserProvider", :dependent => :destroy
   accepts_nested_attributes_for :providers
 
-  authenticates_with_sorcery!
-
+  validates_uniqueness_of :email, :case_sensitive => false, :allow_blank => false
   validates_length_of :password, :minimum => 3, :message => "password must be at least 3 characters long", :if => :password
   validates_confirmation_of :password, :message => "should match confirmation", :if => :password
+
+  authenticates_with_sorcery!
 end
